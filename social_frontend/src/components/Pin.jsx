5c50b16750f291611d07675abd1971c6661ef075
Rodrigo Seminario <rodasemi5@gmail.com>
2023-01-03 18:02:55 -0500

    frontend updates

--- social_frontend/src/components/Pin.jsx
+++ social_frontend/src/components/Pin.jsx
@@ -6,12 +6,18 @@ import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 import { client, urlFor } from '../client';
+import { fetchUser } from '../utils/fetchUser';
 
-const Pin = ({ pin: { postedBy, image, _id, destination}}) => {
+const Pin = ({ pin: { postedBy, image, _id, destination, save}}) => {
 
     const [postHovered, setPostHovered] = useState(false)
     const [savingPost, setSavingPost] = useState(false)
     const navigate = useNavigate()
+
+    const user = fetchUser
+
+    const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId)).length
+  
 
 
   return (
@@ -39,6 +45,15 @@ const Pin = ({ pin: { postedBy, image, _id, destination}}) => {
                                 <MdDownloadForOffline />
                             </a>
                         </div>
+                        {alreadySaved?.length !== 0 ? (
+                            <button>
+                                Saved
+                            </button>
+                        ) : (
+                            <button>
+                                Save
+                            </button>
+                        )}
                     </div>
 
                 </div>
